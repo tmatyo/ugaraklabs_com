aa5c1b129522de1e77ab79d8731d6869cb1e0451
tmatyo <tomi.matyo@gmail.com>
2023-04-13 20:58:40 +0200

    refactoring and styling up the page

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+	<header>
+		<img alt="Ugarak Labs s.r.o." class="logo" src="@/assets/logo.svg" width="200" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+		<div class="wrapper">
+			<nav>
+				<RouterLink :class="navbar-link" to="/">Home</RouterLink>
+				<RouterLink :class="navbar-link" to="/about">About</RouterLink>
+			</nav>
+		</div>
+	</header>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+	<RouterView />
 </template>
 
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -2,22 +2,5 @@
 </script>
 
 <template>
-	<div id="slide1" class="slide">
-		<h1>Time is <span class="hl">money</span>.
-			<br>Even more so if you have 
-			<br>a <span class="hl">business</span> to run.</h1>
-	</div>
-	<div id="slide2" class="slide">
-		<h1 class="ar">For the majority of <span class="hl">people</span>, 
-			<br>a <span class="tp">company</span> without a <span class="hl">website</span> 
-			<br>is not even a <span class="tp">company</span>.</h1>
-	</div>
 
-	<div id="slide3" class="slide">
-		<h1 class="ct">Those <span class="hl">people</span> could be your <span class="hl">clients</span>. Do you already have a <span class="hl">website</span>?</h1>
-		<div class="button-group">
-			<button>Yes</button>
-			<button>No</button>
-		</div>
-	</div>
 </template>
