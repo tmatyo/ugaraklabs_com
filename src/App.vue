dc7150510cd5381cbf7a1f501b123888a72bb85e
tmatyo <tomi.matyo@gmail.com>
2023-04-17 17:13:15 +0200

    left out of previous

--- src/App.vue
+++ src/App.vue
@@ -18,3 +18,38 @@ import UgarakLogo from './components/UgarakLogo.vue';
 	<RouterView />
 </template>
 
+<style>
+
+header {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	height: 70px;
+	padding: 20px;
+}
+
+header .wrapper {
+	display: flex;
+	flex-direction: column;
+	margin-left: auto;
+}
+
+header .wrapper nav a {
+	color: var(--color-text);
+	text-decoration: none;
+	text-transform: uppercase;
+	font-size: 1.5em;
+	font-weight: bold;
+	margin-left: 30px;
+}
+
+header .wrapper nav a:hover {
+	background: -webkit-linear-gradient(69deg, var(--color-brand-1), var(--color-brand-2));
+	-webkit-text-fill-color: transparent;
+	background-clip: text;
+	-webkit-background-clip: text;
+}
+
+
+</style>
+
